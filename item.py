6864f65df30027c2733c648e384e11a2e5fad119
Admin <shivam043@gmail.com>
2018-03-12 02:36:37 +0530

    commit files

---
+++ item.py
@@ -0,0 +1,54 @@
+from flask_restful import Resource,reqparse
+import sqlite3
+from flask_jwt import jwt_required
+
+
+items=[]
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float, required=True, help="cannot be left blanked")
+
+
+    #@jwt_required()
+    def get(self,name):
+        connection=sqlite3.connect('data.db')
+        cursor=connection.cursor()
+
+        query="SELECT * FROM items WHERE name=?"
+        result=cursor.execute(query,(name,))
+        row=result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item':{'name':row[0],'price':row[1]}}
+        return {'message':'Item not found'},404
+
+
+    def post(self,name):
+
+        if next(filter(lambda x:x['name']==name,items),None):
+            return {'message':"an item with {} already exists.".format(name)},400
+        data = Item.parser.parse_args()  #force=True will not check  the content headr #silent=True
+        item={'name':name,'price':data['price']}
+        items.append(item)
+        return item,201
+    def delete(self,name):
+        global items
+        items=list(filter(lambda x:x['name']!=name,items))
+        return {'message':'item deleted'}
+    def put(self,name):
+        data=Item.parser.parse_args()
+        item=next(filter(lambda x:x['name']==name,items),None)
+        if item is None:
+            item={'name':name,'price':data['price']}
+            items.append(item)
+            return items
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return{'items':items}
